65094072d48d736613287da7b854c3f51da93fbc
github username <github email address>
2019-06-05 11:25:25 -0400

    new migration updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_160210) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "username"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
