3b0c026f666dad1d2494d861ae7a8c76dfacab8b
github username <github email address>
2019-06-05 10:14:41 -0400

    added a require_login helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :require_login
 
   def current_user
     User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
   end
 end
