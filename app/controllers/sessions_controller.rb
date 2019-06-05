98d92aa791720f2f66b7599d124c3418761e0236
github username <github email address>
2019-06-05 10:41:24 -0400

    new method for logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,13 @@ class SessionsController < ApplicationController
   def welcome
   end
 
+  #log in
+  def new
+    if logged_in?
+      redirect_to users_path(current_user)
+    else
+      render '/login'  
+    end
+  end
+
 end
