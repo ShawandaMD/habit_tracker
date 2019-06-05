73b386074047c2e070c44db4590a9638444e83fe
github username <github email address>
2019-06-05 11:26:40 -0400

    create method that logs in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,14 @@ class SessionsController < ApplicationController
     if logged_in?
       redirect_to users_path(current_user)
     else
-      render '/login'  
+      render '/login'
     end
+  end
+
+  def create
+    @user =  User.find_by(username: params[:user][:username])
+    return head(:forbidden) unless @user.try(:authenticate, params[:user][:password])
+    session[:user_id] = @user.id
   end
 
 end
