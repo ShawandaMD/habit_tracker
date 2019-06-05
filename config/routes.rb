549c9028600bf72aafc7a4e39b10ca4632901230
github username <github email address>
2019-06-05 11:20:44 -0400

    added routes to login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources :users
 
   root 'sessions#welcome'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy' 
+
 end
