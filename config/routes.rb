428157ff6bc4d8bf029c4e4822131e84dfe25658
github username <github email address>
2019-06-09 15:02:02 -0400

    added :new, :create and :show routes to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :activities
   resources :habits
-  resources :users
+  resources :users, only: [:new, :create, :show]
 
   root 'sessions#welcome'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy' 
+  post '/logout', to: 'sessions#destroy'
 
 end
