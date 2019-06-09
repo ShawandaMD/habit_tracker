8f89c385189605bef0edb5c8d3f1c708cd28d79a
github username <github email address>
2019-06-09 15:48:30 -0400

    redirect to user/:id route in create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,12 @@ class SessionsController < ApplicationController
     @user =  User.find_by(email: params[:user][:email])
     return head(:forbidden) unless @user.try(:authenticate, params[:user][:password])
     session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
+
+  private
+  #def user_params
+  #  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  #end
 
 end
