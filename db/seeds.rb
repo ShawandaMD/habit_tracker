53fc0c15bc2c0df8509a569db241d4eb98b7c26d
github username <github email address>
2019-06-09 15:50:13 -0400

    seed data for 3 Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Users
+user1 = User.create(name: "Emily Payton" ,email: "EmilyP@gmail.com" ,password:"EmilyP")
+user2 = User.create(name: "Chris Moore" ,email: "ChrisM@gmail.com" ,password: "ChrisM")
+user3 = User.create(name: "Erin Hall" ,email: "ErinHall@gmail.com" ,password: "ErinHall")
