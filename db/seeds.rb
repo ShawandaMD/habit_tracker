4ccb3988247e03561a2e5f143f3be6d616c749de
github username <github email address>
2019-06-14 12:40:35 -0400

    changed Users seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 #Users
-user1 = User.create(name: "Emily Payton" ,email: "EmilyP@gmail.com" ,password:"EmilyP")
-user2 = User.create(name: "Chris Moore" ,email: "ChrisM@gmail.com" ,password: "ChrisM")
-user3 = User.create(name: "Erin Hall" ,email: "ErinHall@gmail.com" ,password: "ErinHall")
+user1 = User.create(name: "Emily Payton" ,email: "emily@gmail.com" ,password:"emily")
+user2 = User.create(name: "Chris Moore" ,email: "chris@gmail.com" ,password: "chris")
+user3 = User.create(name: "Erin Hall" ,email: "erin@gmail.com" ,password: "erin")
