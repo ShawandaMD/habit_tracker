066c96c54e97c07c2211a8b13d533047e589b560
github username <github email address>
2019-06-15 22:44:57 -0400

    changed GET verb for logout route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  post '/logout', to: 'sessions#destroy'
 
 end
