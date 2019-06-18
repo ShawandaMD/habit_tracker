0241487dafb6b06dcc990382f5f350cb32b91b74
github username <github email address>
2019-06-18 16:44:49 -0400

    changed column names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_164853) do
+ActiveRecord::Schema.define(version: 2019_06_16_122711) do
 
   create_table "activities", force: :cascade do |t|
     t.string "action"
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 2019_06_11_164853) do
 
   create_table "habits", force: :cascade do |t|
     t.string "name"
-    t.string "type"
-    t.string "summary"
+    t.string "category"
+    t.string "goal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
