5ceb9e71f6c06f2e420d3e54a34eae3ad5ac4bdd
github username <github email address>
2019-06-18 18:53:16 -0400

    created the new method

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,2 +1,23 @@
 class HabitsController < ApplicationController
+  def show
+
+  end
+
+  def new
+    @habit = Habit.new
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  
 end
