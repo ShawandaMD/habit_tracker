044cd3045defa55705848cfaa1c1289d38c02a2d
github username <github email address>
2019-06-19 09:17:28 -0400

    added presence validations for name, category and goal attributes

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,5 +1,9 @@
 class Habit < ActiveRecord::Base
   has_many :activities
-  has_many :events, through: :activities 
+  has_many :events, through: :activities
   belongs_to :user
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :goal, presence: true
 end
