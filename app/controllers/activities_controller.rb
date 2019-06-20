1b312d6cb0e130c6b5d02f240f6799b9127adb12
github username <github email address>
2019-06-20 10:39:00 -0400

    added empty methods for CRUD

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,25 @@
 class ActivitiesController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
 end
