ec6cef36e687705ba0e1295d1a2968c6d6bde7e4
github username <github email address>
2019-06-20 12:33:49 -0400

    added a red box for form errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :habits, only: [:index, :show] do
-    resources :activities
-  end
-
   resources :users, only: [:new, :create, :show] do
     resources :habits
+  end
+
+  resources :habits, only: [:index, :show] do
+    resources :activities
   end
 
   root 'sessions#welcome'
