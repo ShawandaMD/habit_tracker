a19e88519ac6dfaef41d620e52d3670ba4ed5643
github username <github email address>
2019-06-25 09:06:02 -0400

    accepts_nested_attributes_for event model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,8 @@
 class Activity < ActiveRecord::Base
   belongs_to :habit
   belongs_to :event
+  accepts_nested_attributes_for :event, reject_if: :all_blank
 
   ACTION = ["No attempt", "50% completed", "Completed"]
+
 end
