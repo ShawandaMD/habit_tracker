3a5063557ff021d765da4f8b48afcf1ffcc6da78
github username <github email address>
2019-06-25 09:07:44 -0400

    removed nestedaccepts_nested_attributes_for event model

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,6 +2,7 @@ class Habit < ActiveRecord::Base
   has_many :activities
   has_many :events, through: :activities
   belongs_to :user
+  
 
   validates :name, presence: true
   validates :category, presence: true
