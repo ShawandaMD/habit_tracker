3445ff3ccd56ace1ec9b57f39e3318c90cd75320
github username <github email address>
2019-06-26 00:18:34 -0400

    added new data and associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+User.destroy_all
+Activity.destroy_all
+Habit.destroy_all
+Event.destroy_all
+
 #Users
 user1 = User.create(name: "Emily Payton" ,email: "emily@gmail.com" ,password:"emily")
 user2 = User.create(name: "Chris Moore" ,email: "chris@gmail.com" ,password: "chris")
@@ -8,3 +13,20 @@ h1 = user1.habits.create(name: "Mediate for 5 minutes" ,category: "Health" ,goal
 h2 = user1.habits.create(name: "Eat Keto" ,category: "Health" ,goal: "I want to loose 10 pounds" )
 h3 = user1.habits.create(name: "Read 10 pages of Psychology book" ,category: "Education" ,goal: "To finish reading psycology book in 2 months" )
 h4 = user2.habits.create(name: "Stop Smoking" ,category: "Health" ,goal: "Cut down to 1 cigarette a week in a month" )
+
+#events
+e1 = Event.create(occurance: "2019-06-24")
+e2 = Event.create(occurance: "2019-06-23")
+e3 = Event.create(occurance: "2019-06-18")
+e4 = Event.create(occurance: "2019-06-17")
+e5 = Event.create(occurance: "2019-06-16")
+e6 = Event.create(occurance: "2019-06-07")
+
+#Activities
+
+a1 = h1.activities.create(action: "Completed", event_id: 1)
+a2 = h1.activities.create(action: "50% completed", event_id: 2)
+a3 = h1.activities.create(action: "50% completed", event_id: 3)
+a4 = h2.activities.create(action: "Completed", event_id: 4)
+a5 = h2.activities.create(action: "Completed", event_id: 5)
+a6 = h3.activities.create(action: "No attempt", event_id: 6)
