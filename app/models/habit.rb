e768dfec225a62edc52dc454689c73e98a87245e
github username <github email address>
2019-09-04 13:49:24 -0400

    no changes

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,9 +2,15 @@ class Habit < ActiveRecord::Base
   has_many :activities
   has_many :events, through: :activities
   belongs_to :user
-  
+
 
   validates :name, presence: true
   validates :category, presence: true
   validates :goal, presence: true
+
+  def completed_activity_name
+    self.activities.where("action = ?","Completed").map do |act|
+      act.action
+    end  
+  end
 end
