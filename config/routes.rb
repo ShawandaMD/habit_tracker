fafca59b7114834d7749d0ccb4a8f33a8173664f
github username <github email address>
2019-09-09 15:24:54 -0400

    updated logout route to delete http verb

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
 end
