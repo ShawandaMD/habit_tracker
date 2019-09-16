02a7e8a463ca25ff7488fed7e1d59ead35d504b0
github username <github email address>
2019-09-16 14:36:40 -0400

    rendered habits index page through javascript

--- app/assets/javascripts/habit.js
+++ app/assets/javascripts/habit.js
@@ -5,13 +5,40 @@ $(document).ready (function() {
     event.preventDefault()
 		getHabits(event.target.dataset.userId)
 	})
+  $('form').submit(function( event ) {
+  //alert( "Handler for .submit() called." );
+  event.preventDefault();
+  habitsForm(event.target)
+  });
 })
+
+function habitsForm(form) {
+  //debugger
+  fetch(`${form.action}`, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify($(form).serializeArray()), // data can be `string` or {object}!
+  }).then(res => res.json())
+  .then(habitObject => console.log(habitObject))
+  .catch(error => console.error('Error:', error));
+}
 
 function getHabits(id) {
   fetch(`/users/${id}/habits.json`)
   .then((response) => response.json())
-  .then((habitsArray) => console.log(habitsArray));
+  .then((habitsArray) => {
+    listOfHabits(habitsArray)
+  })
 }
+
+function listOfHabits(habitsArray) {
+  //debugger
+  const habits = habitsArray.map(habit => { //create a variable to store the  habits array
+    const obj = new Habit(habit) //create a variable to turn the JSON in to an object
+    const html =  obj.habitsHTML()
+    $('div#habits-list').append(html)
+  })
+};
+
 
 class Habit {
 	constructor(obj) {
@@ -20,3 +47,10 @@ class Habit {
 		this.category = obj.category
 		this.goal = obj.goal
 	}
+
+  habitsHTML() {//es6 syntax instance function(use this keyword to get access)
+    return (`<li>
+      <a href="http://localhost:3000/habits/${this.id}">${this.name}</a>
+    </li>`)
+
+  }};
