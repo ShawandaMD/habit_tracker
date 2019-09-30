27142506518cf492153f73ebe9e17fca01f5f4e2
github username <github email address>
2019-09-29 23:22:46 -0400

    reset my database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_16_122711) do
+ActiveRecord::Schema.define(version: 2019_09_30_013646) do
 
   create_table "activities", force: :cascade do |t|
     t.string "action"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_06_16_122711) do
     t.date "occurance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "comment"
   end
 
   create_table "habits", force: :cascade do |t|
